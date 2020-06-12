49b6f97e158cd0c751bbc26c52352a74b828d4f8
Ibraheem Alqwaiz <ialqwaiz@users.noreply.github.com>
2020-06-12 03:00:24 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 if ! pgrep -f 'osd'
 then
-    nohup osd --cli -i -a -l $LANG /media >> /tmp/log.txt 2>&1
+    #nohup osd --cli -u $OSD_USERNAME -p $OSD_PASSWORD -i -a -l $LANG /media >> /tmp/log.txt 2>&1
+    nohup osd --cli -u $OSD_USERNAME -p $OSD_PASSWORD -i -a -l $LANG /media
 else
-    echo "running" >> ~/out_test.txt
+    #echo "running" >> ~/out_test.txt
+    echo "running"
 fi
